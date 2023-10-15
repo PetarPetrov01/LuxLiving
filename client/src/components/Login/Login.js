5b7e8d639b3e61a63ca9ddb554dce0622901871a
PetarPetrov01 <petyr_p@mail.bg>
2023-10-15 12:42:50 +0300

    Update Login.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -17,3 +17,32 @@ export const Login = (props) => {
             </StyledForm>
     );
 };
+
+/*
+<section id="login">
+            <div className="form">
+                <h2>Login</h2>
+                <form className="login-form" onSubmit={(e) => onLoginSubmit(e, formValues)}>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="email"
+                        onChange={onChangeHandler}
+                        value={formValues.email} />
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="password"
+                        onChange={onChangeHandler}
+                        value={formValues.password}
+                    />
+                    <button type="submit">login</button>
+                    <p className="message">
+                        Not registered? <Link to="/register">Create an account</Link>
+                    </p>
+                </form>
+            </div>
+        </section>
+*/
