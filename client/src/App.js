afe038f6419fc69a2927e2a91641f0859cd4fa95
PetarPetrov01 <petyr_p@mail.bg>
2023-10-15 12:39:38 +0300

    Global styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
+import { GlobalStyle } from './components/GlobalStyles';
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <main>
           <Route></Route>
         </Routes>
---
+++ client/src/components/GlobalStyles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    body{
+        color: rgb(10, 187, 241);
+        background: rgb(205, 235, 255);
+        background-image: 
+            linear-gradient(94deg, rgba(192, 238, 254,0.5) 0%, rgba(126, 218, 255,0.5) 100%),
+            url('/images/luxury-house.webp');
+        background-position: center;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+        background-size: cover;
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+    }
+
+    #root {
+        margin: auto;
+        height: 100vh;
+        width: 100%;
+        top: 0;
+        left: 0;
+        background: transparent;
+    }
+`;
