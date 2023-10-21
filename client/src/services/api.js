a1e357d0bc13c29db5d607c6e669c69e6313b11f
PetarPetrov01 <petyr_p@mail.bg>
2023-10-21 20:30:43 +0300

    Implement request logic with userService

---
+++ client/src/services/api.js
@@ -0,0 +1,44 @@
+import { userStorage } from "./userStorage";
+
+const request = async (method, url, data) => {
+    const options = {
+        method,
+        headers: {}
+    };
+
+    if (data) {
+        options.headers['Content-type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    };
+
+    const user = userStorage.getUser();
+    if (user?.accessToken) {
+        options.headers['X-Authorization'] = user.accessToken;
+    }
+
+    try {
+        const response = await fetch(url, options);
+
+        if (response.ok !== true) {
+            if (response.status === 403) {
+                userStorage.deleteUser();
+            }
+            const error = await response.json();
+            throw error;
+        }
+
+        if (response.status === 204) {
+            return response;
+        }
+        return await response.json();
+
+    } catch (error) {
+        throw error.message;
+    }
+};
+const get = request.bind(null, 'GET');
+const post = request.bind(null, 'POST');
+const put = request.bind(null, 'PUT');
+const del = request.bind(null, 'DELETE');
+
+export { get, post, put, del };
---
+++ client/src/services/userService.js
@@ -0,0 +1,36 @@
+import * as api from './api';
+import { userStorage } from './userStorage';
+
+const baseUrl = 'http://localhost:3030/users';
+
+const login = async (data) => {
+
+    try {
+        const result = await api.post(`${baseUrl}/login`, data);
+        return result;
+
+    } catch (error) {
+        alert(error);
+    }
+};
+
+const register = async (data) => {
+    try {
+        const result = await api.post(`${baseUrl}/register`, data);
+        userStorage.setUserStorage(result);
+
+        return result;
+    } catch (error) {
+        alert(error);
+    }
+};
+
+const logout = async () => {
+    try {
+        await api.get(`${baseUrl}/logout`);
+    } catch (error) {
+        alert(error);
+    }
+};
+
+export const userService = { login, register, logout };
