6b3dacf3e29c3b998f05084ba7a3be40d3525d63
PetarPetrov01 <petyr_p@mail.bg>
2023-10-23 19:04:40 +0300

    User Storage service

---
+++ client/src/services/userStorage.js
@@ -0,0 +1,19 @@
+const setUserStorage = (key, data) => {
+    localStorage.setItem(key, JSON.stringify(data));
+};
+
+const getUser = () => {
+    const user = localStorage.getItem('user');
+    try {
+        const result = user ? JSON.parse(user) : null;
+        return result;
+    } catch (error) {
+        alert('Error from storage');
+    }
+};
+
+const deleteUser = () => {
+    localStorage.clear();
+};
+
+export const userStorage = { setUserStorage, getUser, deleteUser };
