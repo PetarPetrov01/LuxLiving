7de2215f0cfd700888cd45f320c839751479fbc5
PetarPetrov01 <petyr_p@mail.bg>
2023-10-23 19:06:00 +0300

    Implement register

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,17 +1,50 @@
 import { Link } from "react-router-dom";
-import { StyledInput, StyledForm } from "../../styles/Form/StyledForm";
+import { StyledInput, StyledForm, InputWrapper } from "../../styles/Form/StyledForm";
+import { useForm } from "../../hooks/useForm";
+import { useUserContext } from "../../contexts/UserContext";
 
 export const Register = () => {
-    return(
-        <StyledForm>
+
+    const { onRegisterSubmit } = useUserContext();
+    const { formValues, onChangeHandler } = useForm({
+        email: '',
+        password: '',
+        repass: ''
+    });
+
+    return (
+        <StyledForm onSubmit={(e) => onRegisterSubmit(e, formValues)}>
             <h1>Sign up</h1>
-                <StyledInput />
-                <StyledInput />
-                <StyledInput />
-                <button>Register</button>
-                <p>
-                    Not registerd? <Link to={'/login'}>Sign in</Link>
-                </p>
+            <InputWrapper>
+                <StyledInput
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    value={formValues.email}
+                    onChange={onChangeHandler} />
+            </InputWrapper>
+
+            <InputWrapper>
+                <StyledInput
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={formValues.password}
+                    onChange={onChangeHandler} />
+            </InputWrapper>
+
+            <InputWrapper>
+                <StyledInput
+                    type="password"
+                    placeholder="Repeat password"
+                    name="repass"
+                    value={formValues.repass}
+                    onChange={onChangeHandler} />
+            </InputWrapper>
+            <button>Register</button>
+            <p>
+                Not registerd? <Link to={'/login'}>Sign in</Link>
+            </p>
         </StyledForm>
     );
 };
--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -10,8 +10,28 @@ export const UserProvider = ({children}) => {
 
     const [user, setUser] = useLocalStorage('user', {});
     const navigate = useNavigate();
+    const onRegisterSubmit = async (e, data) => {
+        e.preventDefault();
+
+        try {
+            const result = await userService.register(data);
+
+            const user = {
+                email: result.email,
+                _id: result._id,
+                accessToken: result.accessToken
+            };
+
+            setUser(user);
+            navigate('/');
+        } catch (error) {
+            alert(error);
+        }
+    };
+
     const context = {
         user,
+        onRegisterSubmit,
     };
 
     return (
