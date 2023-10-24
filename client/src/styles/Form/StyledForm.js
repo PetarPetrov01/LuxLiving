04f56d4df089c61efc4aebf510e66f532aa4734f
PetarPetrov01 <petyr_p@mail.bg>
2023-10-24 22:13:37 +0300

    dateFormatter and minor form style changes

--- client/src/styles/Form/StyledForm.js
+++ client/src/styles/Form/StyledForm.js
@@ -7,7 +7,7 @@ export const StyledForm = styled.form`
     padding: 40px 20px;
     margin: 20px auto;
     width: 60%;
-    min-height: 60vh;
+    min-height: 55vh;
     border-radius: 15px;
     background: rgba(55, 82, 135,0.8);
     box-shadow: 0px 0px 16px rgb(55, 82, 135);
@@ -28,6 +28,10 @@ export const StyledForm = styled.form`
             background-color: rgba(53, 165, 255, 0.8);
         }
     }
+
+    @media (max-width: 780px){
+        width: 80%;
+    }
 `;
 
 export const InputWrapper = styled.div`
@@ -40,12 +44,16 @@ export const InputWrapper = styled.div`
         color: #f66;
         margin: 0;
     }
+
+    @media (max-width: 780px) {
+            width: 70%;
+    }
 `;
 
 export const StyledInput = styled.input`
     width: 100%;
-    font-size: 1.3em;
     height: 40px;
+    font-size: 1.3em;
     background-color: rgb(220,230,250);
     border-radius: 10px;
     padding-left: 4px;
@@ -53,4 +61,18 @@ export const StyledInput = styled.input`
     &::placeholder{
         color: #999;
     }
+   
 `;
+
+export const StyledTextArea = styled.textarea`
+    width:100%;
+    height: 80px;
+    font-size: 1.3em;
+    background-color: rgb(220,230,250);
+    border-radius: 10px;
+    padding-left: 4px;
+
+    &::placeholder{
+        color: #999;
+    }
+    `;
