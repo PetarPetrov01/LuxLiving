6fd56cdd04a751037fc48a93b3647bdd19ac2532
PetarPetrov01 <petyr_p@mail.bg>
2023-10-27 17:11:49 +0300

    Create custom hook for validating

---
+++ client/src/hooks/usePropertyValidator.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const usePropertyValidator = (initial) => {
+    const [errors, setErrors] = useState(initial);
+
+    const validateForm = (e) => {
+        const value = e.target.value;
+
+        const errMap = {
+            name: () => value.length < 4 && 'Name must be atleast 4 characters long!',
+            location: () => value.length < 4 && 'location must be atleast 4 characters long!',
+            imageUrl: () => !/^https?:\/\/.+/.test(value) && 'Invalid Image URL',
+            price: () => Number(value) < 0.01 && 'price must be a positive number!',
+            area: () => Number(value) < 0.01 && 'Area must be a positive number',
+            description: () => (value.length < 10 || value.length > 200) && 'Description must be between 10 and 200 characters long!',
+        };
+
+        setErrors(prevErrors => ({ ...prevErrors, [e.target.name]: errMap[e.target.name]() }));
+    };
+
+    return {
+        errors,
+        validateForm
+    };
+
+};
