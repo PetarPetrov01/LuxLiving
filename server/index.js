1a1e928f2663e33c58209b96cfa37a2640a78dd5
PetarPetrov01 <petyr_p@mail.bg>
2023-11-02 18:00:07 +0200

    CORS middleware and User model

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const cors = require('./middlewares/cors');
+const userController = require('./controllers/userController');
+
 const connectionString = 'mongodb://localhost:27017/luxliving';
 start();
 
 async function start() {
     await mongoose.connect(connectionString);
     console.log('Connected to database');
+
+    const app = express();
+
     app.use(express.json());
     app.use(cors());
     app.listen(3030, () => console.log('Server started on 3030'));
