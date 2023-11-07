6b4ab0c23086e3f392feef90d2cbe5368f69d886
PetarPetrov01 <petyr_p@mail.bg>
2023-11-07 21:21:09 +0200

    Implement spinner in the details page

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,25 +1,26 @@
 import { useParams } from "react-router-dom";
-import {
-    DetailsCard, DetailsCardHeaders, DetailsImageWrapper, DetailsInfo,
-    DetailsInfoWrapper, StyledDetails
-} from "../../styles/Details/Details.styled";
+import { DetailsCard, DetailsCardHeaders, DetailsImageWrapper, DetailsInfo, DetailsInfoWrapper, StyledDetails } from "../../styles/Details/Details.styled";
 import { useEffect, useState } from "react";
 import { propertyService } from "../../services/propertiesService";
 import { useUserContext } from "../../contexts/UserContext";
 import { formatDate } from "../../utils/dateFormatter";
 import { Controlls } from "./Controlls";
+import { usePropertyContext } from "../../contexts/PropertyContext";
+import { Spinner } from "../Spinner/Spinner";
 import { mockDelay } from "../../utils/mockDelay";
 
 export const Details = () => {
 
+    const { properties } = usePropertyContext();
     const { id } = useParams();
     const [property, setProperty] = useState({});
     const { user } = useUserContext();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         propertyService.getById(id)
             .then(res => {
-                setProperty(res);
                 //Mock delay
                 mockDelay(2000,
                     () => setProperty(res),
@@ -27,38 +28,41 @@ export const Details = () => {
                 );
             }).catch(err => {
                 alert(err);
+                setIsLoading(false);
             });
-    }, [id]);
+    }, [id, properties]);
 
     const isOwner = user._id === property._ownerId;
     const created = formatDate(property.createdAt, 'details');
 
     return (
         <StyledDetails>
-            <DetailsCard>
-                <DetailsImageWrapper>
-                    <img src={property.imageUrl} alt={property.name} />
-                </DetailsImageWrapper>
-                <DetailsInfoWrapper>
-                    <DetailsCardHeaders>
-                        <h1>{property.name}</h1>
-                        <h2>{property.location}</h2>
-                    </DetailsCardHeaders>
-                    <DetailsInfo>
-                        <h4>Total area: {property.area} m<sup>2</sup></h4>
-                        <h4>Current price: ${Number(property.price).toLocaleString()}</h4>
-                        <p>{property.description}</p>
-                    </DetailsInfo>
-                    {user._id ?
-                        <Controlls
-                            isOwner={isOwner}
-                            currentBidder={property.currentBidder}
-                            price={property.price}
-                            propId={property._id} />
-                        : null}
-                    <span>Posted on: {created}</span>
-                </DetailsInfoWrapper>
-            </DetailsCard >
+            {isLoading ? <Spinner /> :
+                <DetailsCard>
+                    <DetailsImageWrapper>
+                        <img src={property.imageUrl} alt={property.name} />
+                    </DetailsImageWrapper>
+                    <DetailsInfoWrapper>
+                        <DetailsCardHeaders>
+                            <h1>{property.name}</h1>
+                            <h2>{property.location}</h2>
+                        </DetailsCardHeaders>
+                        <DetailsInfo>
+                            <h4>Total area: {property.area} m<sup>2</sup></h4>
+                            <h4>Current price: ${Number(property.price).toLocaleString()}</h4>
+                            <p>{property.description}</p>
+                        </DetailsInfo>
+                        {user._id ?
+                            <Controlls
+                                isOwner={isOwner}
+                                currentBidder={property.currentBidder}
+                                price={property.price}
+                                propId={property._id} />
+                            : null}
+                        <span>Posted on: {created}</span>
+                    </DetailsInfoWrapper>
+                </DetailsCard >
+            }
         </StyledDetails >
     );
 
