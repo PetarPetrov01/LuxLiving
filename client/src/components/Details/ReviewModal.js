c1e972032e9379eef3ab507bdbcda870ff487c87
PetarPetrov01 <petyr_p@mail.bg>
2023-11-14 19:07:17 +0200

    ReviewModal Component

--- client/src/components/Details/ReviewModal.js
+++ client/src/components/Details/ReviewModal.js
@@ -3,10 +3,33 @@ import { Modal, RatingContainer, ReviewForm, ReviewWrapper, StyledTextArea, Subm
 export const ReviewModal = ({
     onCloseModal
 }) => {
+    onCloseModal,
+    }) => {
+    const { onCreateReview } = usePropertyContext();
+    const { user: { _id: userId } } = useUserContext();
+
+    const [rating, setRating] = useState(null);
+    const [hoverRating, setHoverRating] = useState(null);
+    const [review, setReview] = useState('');
+
+    const { id } = useParams();
+
+    const onRatingHandler = useCallback((rate) => setRating(rate), []);
+    const onRatingHover = useCallback((rate) => setHoverRating(rate), []);
+
+    const onCloseClick = (e) => {
+        e.preventDefault();
+        onCloseModal();
+    };
     return (
         <Modal>
             <ReviewWrapper>
                 <ReviewForm>
+                        <h3>Review:</h3>
+
+                    <SubmitReviewBtn>
+                        Submit your Review
+                    </SubmitReviewBtn>
                 </ReviewForm>
             </ReviewWrapper>
         </Modal>
