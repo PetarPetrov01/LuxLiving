a0d368965f93e3af29e3bf31321387c4336c8ad6
PetarPetrov01 <petyr_p@mail.bg>
2023-11-14 19:32:51 +0200

    Review Controlled form

--- client/src/components/Details/ReviewModal.js
+++ client/src/components/Details/ReviewModal.js
@@ -1,4 +1,18 @@
 import { Modal, RatingContainer, ReviewForm, ReviewWrapper, StyledTextArea, SubmitReviewBtn, CloseModalButton } from "../../styles/Details/ReviewModal.styled";
+import { useCallback, useState } from "react";
+import { Modal, RatingContainer, ReviewForm, ReviewWrapper, StyledTextArea, SubmitReviewBtn, CloseModalButton, ReviewContainer } from "../../styles/Details/ReviewModal.styled";
+import { Rate } from "./Rate";
+import { usePropertyContext } from "../../contexts/PropertyContext";
+import { useParams } from "react-router-dom";
+import { useUserContext } from "../../contexts/UserContext";
+
+const rateScale = [
+    'Poor',
+    'Fair',
+    'Good',
+    'Great',
+    'Excellent'
+];
 
 export const ReviewModal = ({
     onCloseModal
@@ -21,11 +35,27 @@ export const ReviewModal = ({
         e.preventDefault();
         onCloseModal();
     };
+
+    const handleReviewSubmit = (e) => {
+    };
+
     return (
         <Modal>
             <ReviewWrapper>
                 <ReviewForm>
+                    <ReviewContainer>
                         <h3>Review:</h3>
+                        <StyledTextArea
+                            placeholder="Write your opinion on the property
+                        • What you liked/disliked about it?
+                        • What would you like to know before buying it?
+                        • Would you recommend it?"
+                            onChange={(e) => setReview(e.target.value)}
+                            value={review}
+                        >
+
+                        </StyledTextArea>
+                    </ReviewContainer>
 
                     <SubmitReviewBtn>
                         Submit your Review
