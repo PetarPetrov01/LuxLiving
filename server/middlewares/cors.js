f4f1ea3da3b9130396d3ecb148f9a769210a4f26
PetarPetrov01 <petyr_p@mail.bg>
2023-12-04 00:12:46 +0200

    remove server and keep client only

--- server/middlewares/cors.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    next();
-};
