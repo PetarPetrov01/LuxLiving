8d13c2348317256f07a55f87a885044d2dd4b22b
PetarPetrov01 <petyr_p@mail.bg>
2023-12-07 22:00:32 +0200

    Modify setup

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,8 +1,14 @@
-import {  afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
+import { JSDOM } from 'jsdom';
+const { window } = new JSDOM();
+global.window = window;
+// Mock the alert function
+window.alert = vi.fn();
 
 afterEach(() => {
   cleanup();
 });
+
