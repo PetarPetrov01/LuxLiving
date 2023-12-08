efdc2542267a181957649644f6789d96deb51537
PetarPetrov01 <petyr_p@mail.bg>
2023-12-08 17:34:34 +0200

    Mock Catalog data

---
+++ src/test/mockData.js
@@ -0,0 +1,19 @@
+import { vi } from "vitest";
+
+export const mockCatalogContext = {
+    pages: {
+        "pageCount": 1,
+        "previous": false,
+        "next": false
+    },
+    properties: [{
+        "_id": "1111",
+        "name": "house1",
+        "location": "location1",
+        "area": 51,
+        "description": "Стаята има много боклуци и до леглата има хлебарки. Банята няма тоалетна чиния. Топла вода има рядко.",
+        "price": 100,
+        "rating": 4,
+        "reviews": [],
+    },
+    {
